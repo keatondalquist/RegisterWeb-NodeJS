d476ea0a6643d5e9a14a75dc0485cb8aa7a125c8
fisher-bissett <southpawfish@gmail.com>
2019-02-28 15:47:45 -0600

    commenting out

--- src/routes/employeeDetailRoutes.ts
+++ src/routes/employeeDetailRoutes.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import { RouteLookup } from "../controllers/lookups/stringLookup";
-import * as EmployeeDetailRouteController from "../controllers/employeeDetailRouteController";
+// import express from "express";
+// import { RouteLookup } from "../controllers/lookups/stringLookup";
+// import * as EmployeeDetailRouteController from "../controllers/employeeDetailRouteController";
 
-function employeeDetailRoutes(server: XPathExpression.Express)
-{
-    server.get(RouteLookup.EmployeeDetail, EmployeeDetailRouteController.start);
-	server.get((RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter), EmployeeDetailRouteController.start);
+// function employeeDetailRoutes(server: XPathExpression.Express)
+// {
+//     server.get(RouteLookup.EmployeeDetail, EmployeeDetailRouteController.start);
+// 	server.get((RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter), EmployeeDetailRouteController.start);
 
-	server.put((RouteLookup.API + RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter), EmployeeDetailRouteController.updateEmployee);
+// 	server.put((RouteLookup.API + RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter), EmployeeDetailRouteController.updateEmployee);
 
-	server.post((RouteLookup.API + RouteLookup.EmployeeDetail), EmployeeDetailReouteController.createEmployee);
+// 	server.post((RouteLookup.API + RouteLookup.EmployeeDetail), EmployeeDetailReouteController.createEmployee);
 
-	server.delete((RouteLookup.API + RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter), EmployeeDetailRouteController.deleteEmployee);
+// 	server.delete((RouteLookup.API + RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter), EmployeeDetailRouteController.deleteEmployee);
 
-}
-module.exports.routes = productDetailRoutes;
+// }
+// module.exports.routes = productDetailRoutes;
--- src/routes/employeeListingRoutes.ts
+++ src/routes/employeeListingRoutes.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import { RouteLookup }  from "../controllers/lookups/stringLookup";
-import * EmployeeListingRouteController from "../controllers/employeeListingRouteController";
+// import express from "express";
+// import { RouteLookup }  from "../controllers/lookups/stringLookup";
+// import * EmployeeListingRouteController from "../controllers/employeeListingRouteController";
 
-function employeeListingRoutes(server: express.Express)
-{
-    server.get(RouteLookup.EmployeeListing, EmployeeListingRouteController.start);
+// function employeeListingRoutes(server: express.Express)
+// {
+//     server.get(RouteLookup.EmployeeListing, EmployeeListingRouteController.start);
 
-}
-module.exports.routes = employeeListingRoutes;
+// }
+// module.exports.routes = employeeListingRoutes;
